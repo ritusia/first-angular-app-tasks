f8280bf5fd0744744c36ee56f04ec29340d472c4
jakNatonek <jakub.natonek@houseofangular.io>
2024-09-01 15:46:09 +0200

    rituski praca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from "./dummy-users";
+import {TasksComponent} from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
+  selectedUser: {     id: string;     name: string;     avatar: string; } | null = null;
+
+  onSelectUser(selectedUserId: string) {
+    console.log('User selected: '+ selectedUserId);
+    this.selectedUser = DUMMY_USERS.find(user => user.id === selectedUserId)!;
+  }
 }
