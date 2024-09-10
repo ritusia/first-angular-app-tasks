7c3e52d1824f621aa0e1d488c28567d66c3009d5
jakNatonek <jakub.natonek@houseofangular.io>
2024-09-10 09:31:42 +0200

    feat: init new task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,30 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
 import {TasksComponent} from "./tasks/tasks.component";
+import {NewTaskComponent} from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NewTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
-  selectedUser: {     id: string;     name: string;     avatar: string; } | null = null;
+  selectedUserId?: string;
+  showNewTask = false;
 
-  onSelectUser(selectedUserId: string) {
-    console.log('User selected: '+ selectedUserId);
-    this.selectedUser = DUMMY_USERS.find(user => user.id === selectedUserId)!;
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+
+  onNewTask() {
+    this.showNewTask = true;
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,64 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, output, Output} from '@angular/core';
+import {TaskComponent} from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
+  @Input( {required: true} ) userId!: string;
+  @Input( {required: true} ) name!: string;
+  addTaskClick = output();
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
+
+  onCompleteTask(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  addTask() {
+    this.tasks.push({
+      id: `t${this.tasks.length + 1}`,
+      userId: this.userId,
+      title: 'New task',
+      summary: 'New task summary',
+      dueDate: '2025-12-31',
+    })
+    this.addTaskClick.emit()
+    ;
+
+  }
 }
+
+
+
